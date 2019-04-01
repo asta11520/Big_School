3c132e55d0ce0397f66c90952f98cb19ee42e5d5
Minh <Minh@DESKTOP-3508D5Q>
2019-04-01 15:51:58 +0700

    16/66

---
+++ WebApplication3/Controllers/CoursesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+using WebApplication3.ViewModel;
+
+namespace WebApplication3.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
